cef7a5282eb0fcff41744cf8d18fa919294c0777
J Park <mrparkonline@gmail.com>
2025-09-25 09:40:00 -0400

    made optimizations

--- p5.py
+++ p5.py
@@ -1,9 +1,12 @@
 # git branching tutorial
 num = int(input("Enter a number:"))
-divider = 1
-counter = 0
-while divider < num:
-    if num % divider == 0:
+divider = 2 # changed from 1
+counter = 1 # we always have 1 as a factor
+#while divider < num:
+while divider < int(num**0.5) + 1:
+    if num % divider == 0 and divider != num // divider:
+        counter += 2
+    elif num % divider == 0:
         counter += 1
     divider += 1
 print(f"{num} has {counter} proper factors.")
